ef047ddced19467c0499c61a887016b20f15b080
Josh <joshhbyron@gmail.com>
2024-06-10 15:43:57 -0400

    robots.txt, meta, google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" />
         {isProduction && (
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5566603516601048"
-            crossorigin="anonymous"
-          ></script>
+          <>
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5566603516601048"
+              crossorigin="anonymous"
+            ></script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7Z93C23QW"></script>
+            <script>
+              window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-C7Z93C23QW');
+            </script>
+          </>
         )}
+
+        <link rel="icon" href="/favicon.svg" />
+        <meta
+          name="description"
+          content="The Word. Use your word association skills to guess the hidden word with feedback on semantic similarity. Can you find the word at position 0?"
+        />
       </Head>
       <body>
         <Main />
