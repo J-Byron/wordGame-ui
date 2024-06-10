3922e776063c79252057002aa5396ed1788f9d09
Josh <joshhbyron@gmail.com>
2024-06-10 16:15:19 -0400

    :/

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function Document() {
-  const isProduction = process.env.NODE_ENV === "production";
   return (
     <Html lang="en">
       <Head>
-        {isProduction && (
-          <>
-            <script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5566603516601048"
-              crossorigin="anonymous"
-            ></script>
-            <GoogleAnalytics gaId="G-C7Z93C23QW" />
-          </>
-        )}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5566603516601048"
+          crossorigin="anonymous"
+        ></script>
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`}
+        />
+        <Script id="google-analytics">
+          {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}');
+          `}
+        </Script>
 
         <link rel="icon" href="/favicon.svg" />
         <meta
